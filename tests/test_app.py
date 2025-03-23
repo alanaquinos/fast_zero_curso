0bdd299a854907683268e4eca6e37af573472d82
Alan Sousa <alanaquino.eng@hotmail.com>
2025-03-23 18:05:01 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -39,9 +39,10 @@ def test_read_users_with_users(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -52,11 +53,32 @@ def test_update_user(client, user):
     assert response.json() == {
         'username': 'bob',
         'email': 'bob@example.com',
-        'id': 1,
+        'id': user.id,
     }
 
 
-def test_update_integrity_error(client, user):
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in token
+    assert 'token_type' in token
+
+
+def test_update_integrity_error(client, user, token):
     # Criando um registro para "fausto"
     client.post(
         '/users',
@@ -70,6 +92,7 @@ def test_update_integrity_error(client, user):
     # Alterando o user.username das fixture para fausto
     response_update = client.put(
         f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'fausto',
             'email': 'bob@example.com',
@@ -81,12 +104,6 @@ def test_update_integrity_error(client, user):
     assert response_update.json() == {
         'detail': 'Username or Email already exists'
     }
-
-
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'User deleted'}
 
 
 def test_get_user_should_return_not_found__exercicio(client):
